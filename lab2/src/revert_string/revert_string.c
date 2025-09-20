29e33d7dfcd3189b4d64b08636d9169978804912
dashanesterova2809-design <dasha.nesterova.2809@gmail.com>
2025-09-20 07:53:29 +0000

    lav2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str), i = 0;
+    int j = len - 1;
+    
+    while (i < j) {
+        char tot = str[i];
+        str[i] = str[j];
+        str[j] = tot;
+        i++;
+        j--;
+    }
 }
 
